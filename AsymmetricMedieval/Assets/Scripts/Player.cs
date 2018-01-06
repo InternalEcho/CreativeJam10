6b7522b8f67ba40ead98496230e63126dd8cc16b
InternalEcho <24857190+InternalEcho@users.noreply.github.com>
2018-01-06 11:15:39 -0500

    Updated .gitignore and added Player script and Player prototype scene.

---
+++ AsymmetricMedieval/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float playerSpeed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(x, 0f, z);
+
+        this.transform.LookAt(this.transform.position + direction);
+        this.transform.Translate(direction * playerSpeed, Space.World);
+    }
+}
