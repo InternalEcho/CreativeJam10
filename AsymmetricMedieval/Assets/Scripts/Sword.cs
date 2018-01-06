6744353213595c9e1a160119d7bb6922b9f0185f
InternalEcho <24857190+InternalEcho@users.noreply.github.com>
2018-01-06 18:34:19 -0500

    Sword Animations completed.

---
+++ AsymmetricMedieval/Assets/Scripts/Sword.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Stab();
+        HorizontalSwing();
+	}
+
+    void Stab()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Stabbed");   //Debug
+            this.GetComponent<Animator>().SetTrigger("Stab");
+        }
+    }
+
+    /*void VerticalSwing()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Debug.Log("Vertical Swing");   //Debug
+            this.GetComponent<Animator>().SetTrigger("VerticalSwing");
+        }
+    }*/
+
+    void HorizontalSwing()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Debug.Log("Horizontal Swing");   //Debug
+            this.GetComponent<Animator>().SetTrigger("HorizontalSwing");
+        }
+    }
+}
