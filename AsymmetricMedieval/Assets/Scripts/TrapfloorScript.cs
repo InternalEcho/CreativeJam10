9967f1c5d3dc466cec28e636e5d4a361e170553b
BrandonCHW <brad_1800@hotmail.com>
2018-01-06 18:30:56 -0500

    Create trap prefab

---
+++ AsymmetricMedieval/Assets/Scripts/TrapfloorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapfloorScript : MonoBehaviour {
+
+    public Transform playerPosition;
+    public float activationTime = 0.5f;
+    public GameObject trapTrigger;
+    public GameObject trapBall;
+    
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "Cube")
+     //   if (trapTrigger.GetComponent<Trigger>())
+        {
+            Debug.Log("activate trap!");
+            StartCoroutine(ActivateTrap());
+        }
+    }
+
+    IEnumerator ActivateTrap()
+    {
+        yield return new WaitForSeconds(activationTime);
+        trapBall.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+}
