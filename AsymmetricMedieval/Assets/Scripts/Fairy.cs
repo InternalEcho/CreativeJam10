65c27ae5f687785ad196157869df7283b90a308c
Kaixiang <kaixiangmu@gmail.com>
2018-01-06 13:41:05 -0500

    fairy basics done

--- AsymmetricMedieval/Assets/Scripts/Fairy.cs
+++ AsymmetricMedieval/Assets/Scripts/Fairy.cs
@@ -8,11 +8,33 @@ public class Fairy : MonoBehaviour {
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+    
+    // Update is called once per frame
+    void Update () {
+        turnToMouse();
+        move();
+    }
+
+    void move()
+    {
         Vector3 temp = Input.mousePosition;
         temp.z = 2f; // Set this to be the distance you want the object to be placed in front of the camera.
         this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+    }
+
+    void turnToMouse()
+    {
+        Vector3 position = transform.position;
+        position.z += 30;
+
+        Plane ground = new Plane(Vector3.forward, position);
+        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float rayLength;
+        if (ground.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            transform.LookAt(pointToLook);
+        }
+        Debug.DrawLine(cameraRay.origin, cameraRay.GetPoint(rayLength), Color.black);
     }
 }
