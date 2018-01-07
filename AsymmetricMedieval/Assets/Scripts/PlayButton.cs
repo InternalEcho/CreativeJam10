5cd942314eba43b5bb876ed9ebcf51feb887f3ec
BrandonCHW <brad_1800@hotmail.com>
2018-01-06 21:16:13 -0500

    Revamp menu

---
+++ AsymmetricMedieval/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour {
+
+	public void GoToGame (int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    
+}
