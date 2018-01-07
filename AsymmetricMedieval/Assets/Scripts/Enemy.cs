99f3bb7a50e54a30a0f80bfdb751c737d7ae9baa
shi <shi@shis-air.wifi.mtl.nad>
2018-01-07 02:37:49 -0500

    A lot of animation

--- AsymmetricMedieval/Assets/Scripts/Enemy.cs
+++ AsymmetricMedieval/Assets/Scripts/Enemy.cs
@@ -11,6 +11,10 @@ public class Enemy : MonoBehaviour {
 	public RaycastHit target;
 	public bool canMove;
 	public bool seeWallOnce;
+	public Animator animator;
+
+	public float jumpDistance;
+	public bool inDamageRange;
 
 	public Vector3 playerdirection(){
 		Vector3 direction = player.transform.position - transform.position;
@@ -25,7 +29,13 @@ public class Enemy : MonoBehaviour {
 		CheckifObstacle ();
 		if (target.transform.CompareTag ("Player")) {
 			Debug.Log ("work");
-			canMove = true;
+			if (Vector3.SqrMagnitude (player.transform.position - transform.position) < jumpDistance) {
+				inDamageRange = true;
+				canMove = false;
+			} else {
+				inDamageRange = false;
+				canMove = true;
+			}
 			seeWallOnce = false;
 		} else if (target.collider.gameObject.layer == 8) {
 			if(!seeWallOnce)
@@ -35,7 +45,7 @@ public class Enemy : MonoBehaviour {
 	}
 
 	IEnumerator AttentionSpan(){
-		move ();
+		canMove = true;
 		yield return new WaitForSeconds (2.0f);
 		seeWallOnce = true;
 	}
@@ -50,18 +60,32 @@ public class Enemy : MonoBehaviour {
 		transform.LookAt(player.transform);
 		//move.
 		transform.Translate(playerdirection().normalized * Time.deltaTime, Space.World);
+
+	}
+		
+	void damagePlayer(){
+		//player.GetComponent<Player> ().loseHp ();
 	}
 
 	// Use this for initialization
 	void Start () {
 		seeWallOnce = true;
+		inDamageRange = false;
 	}
 
 	// Update is called once per frame
 	void Update () {
+
+
 		DetectPlayer ();
 		if(canMove){
 			move();
-		}	
+		}
+		animate ();
+	}
+
+	void animate(){
+		animator.SetBool("walk", canMove);
+		animator.SetBool("jump", inDamageRange);
 	}
 }
